a216abb36eb914c0e48273245d3dd8188c888caf
SloMo128 <nesci.alessandro.17@outlook.it>
2024-08-01 14:12:44 +0200

    123

--- angular-front-end/src/app/app.module.ts
+++ angular-front-end/src/app/app.module.ts
@@ -10,12 +10,10 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { GlobalHttpInterceptorService } from './Service/global-http-Interceptor.service';
 
 import { PageNotFoundComponent } from './Pages/Comments/Page-not-found/page.not.found.component';
+
 import { PostListComponent } from './Pages/Post/Post-List/post-list.component';
 import { PostEditComponent } from './Pages/Post/Post-Edit/post-edit.component';
 import { PostUpdateComponent } from './Pages/Post/Post-Update/post-update.component';
-import { CommentListComponent } from './Pages/Comments/comment-list/comment-list.component';
-import { CommentEditComponent } from './Pages/Comments/comment-edit/comment-edit.component';
-import { CommentUpdateComponent } from './Pages/Comments/comment-update/comment-update.component';
 
 
 
@@ -25,10 +23,7 @@ import { CommentUpdateComponent } from './Pages/Comments/comment-update/comment-
     PageNotFoundComponent,
     PostListComponent,
     PostEditComponent,
-    PostUpdateComponent,
-    CommentListComponent,
-    CommentEditComponent,
-    CommentUpdateComponent
+    PostUpdateComponent
   ],
   imports: [
     BrowserModule,
